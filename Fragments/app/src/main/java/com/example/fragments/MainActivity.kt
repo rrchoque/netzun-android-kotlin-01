d3e4ffdf8ff86d947a626d41c6e90a4bc90c34a8
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-12-24 06:55:51 -0400

    Se terminó el módulo 6: Navegación uso de Fragments

---
+++ Fragments/app/src/main/java/com/example/fragments/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var btnFragment1: Button
+    lateinit var btnFragment2: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnFragment1 = findViewById(R.id.buttonF1)
+        btnFragment2 = findViewById(R.id.buttonF2)
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        val mainFragment = MainFragment()
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+
+        fragmentTransaction.add(R.id.containerFragment, mainFragment)
+        fragmentTransaction.commit()
+
+        btnFragment1.setOnClickListener {
+            Toast.makeText(this, "Fragment 1 presionado", Toast.LENGTH_SHORT).show()
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.containerFragment, firstFragment)
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
+
+        btnFragment2.setOnClickListener {
+            Toast.makeText(this, "Fragment 2 presionado", Toast.LENGTH_SHORT).show()
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.containerFragment, secondFragment)
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
+    }
+}
