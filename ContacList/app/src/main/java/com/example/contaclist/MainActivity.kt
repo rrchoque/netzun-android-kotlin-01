cb568db986c124ad693f77ed5d7a471b5f1b3e61
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-12-31 06:49:24 -0400

    Se terminó el módulo 10: Conectarnos a Internet

--- ContacList/app/src/main/java/com/example/contaclist/MainActivity.kt
+++ ContacList/app/src/main/java/com/example/contaclist/MainActivity.kt
@@ -2,12 +2,17 @@ package com.example.contaclist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var recyclerView: RecyclerView
+    lateinit var dataModelArrayList: ArrayList<Contact>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +24,31 @@ class MainActivity : AppCompatActivity() {
             Contact("Ivan", "34634634")
         )
 
+        dataModelArrayList = ArrayList()
         recyclerView = findViewById(R.id.contact_recyclerView)
 
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = ContactAdapter(contacts)
+
+        // consultas a internet
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://jsonplaceholder.typicode.com/users"
+        val jsonArrayRequest = JsonArrayRequest(url,
+            {
+                response -> Log.i("Volley", "response: $response")
+                for (i in 0 until response.length()){
+                    val item = response.getJSONObject(i)
+                    Log.i("Volley", "item: $item")
+                    dataModelArrayList.add(Contact(item.get("name").toString(), item.get("phone").toString()))
+                }
+                recyclerView.adapter = ContactAdapter(dataModelArrayList)
+                recyclerView.layoutManager = LinearLayoutManager(this)
+            },
+            {
+                error -> error.printStackTrace()
+            })
+
+        queue.add(jsonArrayRequest)
+
     }
 }
